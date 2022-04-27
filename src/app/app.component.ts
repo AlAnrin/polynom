6e00180af1d60731ae3f754f7a0b08e909f7f9b3
AlAnrin <lianirvi@gmail.com>
2022-04-27 08:37:52 +0300

    add counting and some view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,119 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'polynom';
+  points: {x: number, y: number}[] = [
+    // {x: 1, y: 5},
+    // {x: 2, y: 15},
+    // {x: 3, y: 8},
+    // {x: 4, y: 22}
+  ];
+  res = {
+    a: 0,
+    b: 0,
+    c: 0,
+    d: 0
+  };
+
+  resultStr = '';
+
+  pointCounts: {x: number, y: number}[] = [];
+
+  constructor() {
+  }
+
+  addPoint(): void {
+    this.points = [
+      ...this.points,
+      {x: 0, y: 0}
+    ];
+  }
+
+  addCountPoint(): void {
+    this.pointCounts = [
+      ...this.pointCounts,
+      {x: 0, y: 0}
+    ];
+  }
+
+  deleteCountPoint(i: number): void {
+    this.pointCounts = [
+      ...this.pointCounts.slice(0, i),
+      ...this.pointCounts.slice(i + 1)
+    ];
+  }
+
+  count(): void {
+
+    let diff: number[][] = [];
+    for (const point of this.points) {
+      const lev = [];
+      lev.push(Math.pow(point.x, 3));
+      lev.push(Math.pow(point.x, 2));
+      lev.push(+point.x);
+      lev.push(1);
+      lev.push(+point.y);
+
+      diff.push(lev);
+    }
+
+    console.log(diff);
+
+    const one = diff[0].map((x, i) => x - diff[1][i]);
+    const two = diff[3].map((x, i) => x - diff[2][i]);
+    const three = diff[2].map((x, i) => x - diff[1][i]);
+
+    console.log(one, two, three);
+    let twoDiff: number[] = [];
+    if (one[2] === two[2]) {
+      twoDiff = one.map((x, i) => x - two[i]);
+    }
+    else {
+      const coef = one[2]/two[2];
+      console.log(coef);
+      twoDiff = one.map((x, i) => x - coef * two[i]);
+    }
+    console.log(twoDiff);
+    let threeDiff: number[] = [];
+    if (three[2] === two[2]) {
+      threeDiff = three.map((x, i) => x - two[i]);
+    }
+    else {
+      const coef = three[2]/two[2];
+      console.log(coef);
+      threeDiff = three.map((x, i) => x - coef * two[i]);
+    }
+    console.log(threeDiff);
+
+    let aFind = [];
+    if (twoDiff[1] === threeDiff[1]) {
+      aFind = twoDiff.map((x, i) => x - threeDiff[i]);
+    }
+    else {
+      const coef = twoDiff[1]/threeDiff[1];
+      console.log(coef);
+      aFind = twoDiff.map((x, i) => x - coef * threeDiff[i]);
+    }
+
+    console.log(aFind);
+
+    this.res.a = aFind[4]/aFind[0];
+    this.res.b = (twoDiff[4] - twoDiff[0] * this.res.a) / twoDiff[1];
+    this.res.c = (one[4] - one[0] * this.res.a - one[1] * this.res.b) / one[2];
+    this.res.d = diff[0][4] - diff[0][0] * this.res.a - diff[0][1] * this.res.b - diff[0][2] * this.res.c;
+
+    console.log(this.res);
+
+    this.resultStr = `y = ${this.res.a} * x^3 ${this.res.b < 0 ? '-' : '+'} ${Math.abs(this.res.b)} * x^2 ${this.res.c < 0 ? '-' : '+'} ${Math.abs(this.res.c)} * x ${this.res.d < 0 ? '-' : '+'} ${Math.abs(this.res.d)}`;
+  }
+
+  countPoint(i: number): void {
+    const x = this.pointCounts[i].x;
+
+    const y = this.res.a * Math.pow(x, 3) + this.res.b * Math.pow(x, 2) + this.res.c * x + this.res.d;
+    this.pointCounts = [
+      ...this.pointCounts.slice(0, i),
+      {x, y},
+      ...this.pointCounts.slice(i + 1)
+    ];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,39 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { ru_RU } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import ru from '@angular/common/locales/ru';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NzInputModule} from "ng-zorro-antd/input";
+import {NzButtonModule} from "ng-zorro-antd/button";
+import {NzIconModule} from "ng-zorro-antd/icon";
+import {NzCardModule} from "ng-zorro-antd/card";
+import {NzGridModule} from "ng-zorro-antd/grid";
+import {NzLayoutModule} from "ng-zorro-antd/layout";
+
+registerLocaleData(ru);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        NzInputModule,
+        NzButtonModule,
+        NzIconModule,
+        NzCardModule,
+        NzGridModule,
+        NzLayoutModule
+    ],
+  providers: [{ provide: NZ_I18N, useValue: ru_RU }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
